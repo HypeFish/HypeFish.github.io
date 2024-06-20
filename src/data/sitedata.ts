0048d31b1df47c9145833067949d5bfeea696015
HypeFish <99514952+HypeFish@users.noreply.github.com>
2024-06-20 15:49:49 -0400

    fix style

---
+++ src/data/sitedata.ts
@@ -0,0 +1,11 @@
+export const Site = {
+  title: "My Website",
+};
+
+export const Navigation = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "/about" },
+  { name: "Portfolio", link: "/portfolio" },
+  { name: "Blog", link: "/blog" },
+  { name: "Contact", link: "/contact" },
+];
