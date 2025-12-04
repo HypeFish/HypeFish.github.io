f4f57ba2626735e7edcc9cd488ad0da42f718533
Skye Toral <toral.l@northeastern.edu>
2025-12-04 16:07:18 -0500

    small update for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,12 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  // Add the vite configuration below
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      }
+    }
+  }
 });
--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,15 +1,27 @@
 // src/data/projectData.ts
 export const projects = [
     {
+      title: "MyOS",
+      description: "MyOS is a simple operating system built from scratch using C and Assembly language. " +
+      "It features a basic kernel, memory management, a simple file reader, and a command-line interface. This project was created to learn about low-level programming and operating system concepts.",
+      link: "https://github.com/HypeFish/myos"
+    },
+    {
+      title: "Dolphin Cove",
+      description: "Dolphin Cove is a web application that provides a social platform for people to post questions, watch youtube videos together, " +
+      "chat and play games, all in real-time. Built with React, Node.js, Express, and Socket.io, this application allows users to create accounts, join rooms, " +
+      "and interact with each other through text and video. Check it out and become a part of the community!",
+      link: "https://dolphin-cove-project.onrender.com/"
+    },  
+    {
       title: "Asteriods - Reinforcement Learning",
       description: "This is a project that uses the OpenAI package Gymasium, Pytorch, and Numpy to teach an Artificial Agent" +
       " how to play the classic arcade game Asteroids. You can watch the model play at the following link!",
       link: "/video",
-      
     },
     {
       title: "HypeRate4Health",
-      description: "Heres a Java application that connects to the HypeRate API, which conects to heart rate monitors. The application" +
+      description: "This is a Java application that connects to the HypeRate API, which conects to heart rate monitors. The application" +
       " opens up as a native application and it's designed to keep track of an individual's heart rate and save it to a CSV file" + 
       " to be used in data analysis.",
       link: "https://hyperate4health.netlify.app/"
